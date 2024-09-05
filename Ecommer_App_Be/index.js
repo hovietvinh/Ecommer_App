949ab15e11cbbb19a54291370cee8f69f62d5d7f
hovietvinh <hovietvinh29032004@gmail.com>
2024-09-05 21:47:51 +0700

    initial database

--- Ecommer_App_Be/index.js
+++ Ecommer_App_Be/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 require("dotenv").config()
 const clientRoutes = require("./routes/client/index.route") // import client routes
+
+//Connect db mongoose
+const database = require("./config/database")
+database.connect();
+
+
 const app = express()
 const port = process.env.PORT
 
---
+++ Ecommer_App_Be/models/product.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+const productSchema = new mongoose.Schema(
+    {
+        title:String,
+        description:String,
+        price:Number,
+        discountPercentage: Number,
+        stock: Number,
+        thumbnail: String,
+        status: String,
+        position: Number,
+        deleted: {
+            type:Boolean,
+            default:false
+        },
+    }
+)
+
+const Product = mongoose.model("Product",productSchema,"products")
+
+module.exports = Product
